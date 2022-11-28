173f0b7ba60e111f5fb75e7320796b89f552a94f
pierslabs <pedrolosasp@gmail.com>
2022-11-28 19:13:09 +0100

    Sengrid test send email.

---
+++ index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import sengrid from '@sendgrid/mail'
+
+dotenv.config();
+
+sengrid.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'receiver@gmail.com', // Change to your recipient
+  from: 'sender@gmail.com', // Change to your verified sender
+  subject: 'Hellooouu beaby',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>Prueba sengrid js</strong>',
+}
+
+sengrid.send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
